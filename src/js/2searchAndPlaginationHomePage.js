decc81d79e5a8a833e6ff77224b3df727f23a958
alinaYamkova <alyamkova@yahoo.com>
2021-02-03 16:58:23 +0200

    додає пагінатор, розмітку й стилі накнопки

--- src/js/2searchAndPlaginationHomePage.js
+++ src/js/2searchAndPlaginationHomePage.js
@@ -1,12 +1,14 @@
-// import notify from './data/pnotify';
+import notify from './data/pnotify';
+import { paginator } from "pagination";
+// import filmGallaryItem from '../templates/filmGallaryItem.hbs';
+const gallery = document.querySelector('.film-gallery');
+const paginatorBtn = document.querySelector('.paginator-page');
 
 export default {
   baseUrl: 'https://api.themoviedb.org/3/',
   API_KEY: '72466121c9676fc22348299f38033287',
   query: '',
-  guest_session: '',
   pageNumber: 1,
-  perPage: 20,
   language: 'en-US',
   
   get queryValue() {
@@ -15,12 +17,11 @@ export default {
   set queryValue(val) {
     return (this.query = val);
   }, 
-
   setPage() {
-    return this.page += 1;
+    return this.pageNumber += 1;
   },
   resetPage() {
-    return this.page = 1;
+    return this.pageNumber = 1;
   },
   
   fetchFilms(inputValue) {
@@ -31,13 +32,16 @@ export default {
     const url = `${this.baseUrl}${params}`;
     return fetch(url)
     .then((res) => res.json())
-    .then(({ results }) => {
-      return results})
+    .then((result) => {
+      createCardFunc(result);
+      renderFilms = result.results;
+      return renderFilms; 
+    });
   },
-  
+ //--------- --------------------------------------------------------------------------------------------
   fetchGenres() {
     let genres = '';
-    const params = `genre/movie/list?api_key=${this.API_KEY}&language=${this.language}&page=${this.page}`;
+    const params = `genre/movie/list?api_key=${this.API_KEY}&language=${this.language}&page=${this.pageNumber}`;
     const url = `${this.baseUrl}${params}`;
     return fetch(url)
     .then((res) => res.json())
@@ -45,25 +49,52 @@ export default {
       data.forEach(({data}) => {
         genres += data[i].name;
         console.log(genres);
-        return genres;
       });
-    });
+      return genres;
+    })
   },
-
+//---------------------------------------------------------------------------------------------
   fetchPopularsMoviesList(val) {
     this.pageNumber = val;
-    const params = `movie/popular?api_key=${this.API_KEY}&language=${this.language}&page=${this.page}`;
+    const params = `movie/popular?api_key=${this.API_KEY}&language=${this.language}&page=${this.pageNumber}`;
     const url = `${this.baseUrl}${params}`;
     return fetch(url)
     .then((res) => res.json())
-    .then(({ results }) => {
-      return results
-    })
+    .then((results ) => results)
+    .then((result) => {
+      createCardFunc(result);
+      renderFilms = result.results;
+      return renderFilms;    
+    });
   },
+  
+
+//   onLoadMore(e) {
+//     e.preventDefault();
+//     fetchFilms(this.query, this.pageNumber);
+//     setPage();
+//     onPaginator();
+//   },
 };
 
-///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+function createCardFunc(result) {
+  if(result.results.length === 0) {
+    notify.noticeMessage();
+    return;
+  };
+    const card = filmDetails(result);
+    gallery.insertAdjacentHTML("beforeend", card);
+    window.scrollTo({
+      top: document.documentElement.scrollHeight, behavior: 'smooth'
+    });
+    // onLoadMore();
+    paginator();
+    return gallery;
+};
 
+
+//-----------------------------------------------------------------------------------------
 form.addEventListener('submit', searchFilms);
 
 function searchFilms(e) {
@@ -75,10 +106,9 @@ function searchFilms(e) {
     elem.classList.remove('hidden');
     return;
   }; 
-  resetPage();
-  fetchFilms(inputValue).then(createCardFunc).catch(notify.errorMessage); 
-  // input.value = "";
-  // gallery.innerHTML = '';
-}
-
-
+    resetPage();
+    fetchFilms(inputValue).catch(notify.errorMessage); 
+    elem.classList.add('hidden');
+    inputValue = "";
+    gallery.innerHTML = '';
+};
