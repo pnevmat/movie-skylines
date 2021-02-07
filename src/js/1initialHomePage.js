4ed9327e7b3fc35050f4e15bd2075b16b0050478
ronin314 <maxim.kom1992@gmail.com>
2021-02-07 20:49:54 +0200

    Update 1initialHomePage.js

--- src/js/1initialHomePage.js
+++ src/js/1initialHomePage.js
@@ -4,51 +4,17 @@ import api from './apiService';
 
 let renderFilms = [];
 let genres = [];
+let pageNumber = 1;
 
 const filmGalleryRef = document.querySelector('.film-gallery');
 
-// const apiService = {
-//   baseUrl: 'https://api.themoviedb.org/3/',  
-//   API_KEY: '72466121c9676fc22348299f38033287',
-//   query: '',
-//   // guest_session: '',
-//   // pageNumber: 1,
-//   // perPage: 20,
-//   language: 'en-US',
-
-//   get queryValue() {
-//     return this.query;  
-//   },
-//   set queryValue(val) {
-//     return (this.query = val);  
-//   },
-
-//   fetchFilms(inputValue, pageNumber) {
-//     if (inputValue && inputValue.length > 0) {
-//       this.queryValue = inputValue;  
-//     }
-//     const params = `search/movie?api_key=${this.API_KEY}&language=${this.language}&page=${pageNumber}&include_adult=false&query=${this.queryValue}`;
-//     const url = `${this.baseUrl}${params}`;
-//     return fetch(url)
-//       .then(res => res.json())
-//       .then(({ results }) => {
-//         return results;  
-//       });
-//   },
-// };
-
-let pageNumber = 1;
-
 const createCardFunc = arr => {
   const galleryItemMarkup = filmGalleryTpl(arr);
-  console.log(galleryItemMarkup);
-  // find target
-  // filmGalleryRef.addEventListener('click', activeDetailsPage(movieId, false));
   return galleryItemMarkup;
-};  
+};
 
 const fetchPopularMoviesList = () => {
-  const url = `https://api.themoviedb.org/3/movie/popular?api_key=72466121c9676fc22348299f38033287&language=en-US&page=1`;
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${api.API_KEY}&language=en-US&page=1`;
   return fetch(url)
     .then(response => response.json())
     .then(data => {
@@ -57,29 +23,15 @@ const fetchPopularMoviesList = () => {
       filmGalleryRef.insertAdjacentHTML('beforeend', fragment);
     });
 };
-  return (
-    fetch(url)
-      .then(response => response.json())
-      // .then(({ movies }) => movies)
-      .then(movies => {
-        console.log(movies.results);
-        const fragment = createCardFunc(movies.results);
-        filmGalleryRef.insertAdjacentHTML('beforeend', fragment);
-        return renderFilms.push(movies.results)
-      })  
-  );    
-};
 
 const fetchGenres = () => {
   const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${api.API_KEY}&language=en-US`;
   return fetch(url)
     .then(response => response.json())
     .then(({ genres }) => genres);
-};    
+};
 
 fetchPopularMoviesList();
 fetchGenres();
 
-
-
-export default { createCardFunc }
+export default { createCardFunc };
