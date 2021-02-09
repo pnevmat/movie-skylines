fb086d0e38b5c13fcd3c0a0e6cef69a860b85e69
ronin314 <maxim.kom1992@gmail.com>
2021-02-09 21:23:52 +0200

    homePage

--- src/js/1initialHomePage.js
+++ src/js/1initialHomePage.js
@@ -3,9 +3,31 @@ import filmGalleryTpl from '../templates/filmGalleryItem.hbs';
 import ApiService from './apiService';
 import notify from './data/pnotify';
 import pagesRenderHandler from './2searchAndPlaginationHomePage';
+import moment from 'moment';
 
 let renderFilms = [];
-let genres = [];
+// let genres = [];
+const allGenres = [
+  { id: 28, name: 'Action' },
+  { id: 12, name: 'Adventure' },
+  { id: 16, name: 'Animation' },
+  { id: 35, name: 'Comedy' },
+  { id: 80, name: 'Crime' },
+  { id: 99, name: 'Documentary' },
+  { id: 18, name: 'Drama' },
+  { id: 10751, name: 'Family' },
+  { id: 14, name: 'Fantasy' },
+  { id: 36, name: 'History' },
+  { id: 27, name: 'Horror' },
+  { id: 10402, name: 'Music' },
+  { id: 9648, name: 'Mystery' },
+  { id: 10749, name: 'Romance' },
+  { id: 878, name: 'Science Fiction' },
+  { id: 10770, name: 'TV Movie' },
+  { id: 53, name: 'Thriller' },
+  { id: 10752, name: 'War' },
+  { id: 37, name: 'Western' },
+];
 let pageNumber = 1;
 const api = new ApiService();
 
@@ -24,34 +46,43 @@ const createCardFunc = (number, arr) => {
 };
 
 function firstPageRenderHandler(number, arr) {
-  const moviesToRender = arr.filter((item, index) => index <= (number * 9 - 1));
+  const moviesToRender = arr.filter((item, index) => index <= number * 9 - 1);
   renderCardHandler(moviesToRender);
-};
+}
 
 function otherPagesRenderHandler(number, arr) {
-  const moviesToRender = arr.filter((item, index) => index >= (number * 9 - 1) - 8 && index <= (number * 9 - 1));
+  const moviesToRender = arr.filter(
+    (item, index) => index >= number * 9 - 1 - 8 && index <= number * 9 - 1,
+  );
   renderCardHandler(moviesToRender);
-};
+}
 
 function renderCardHandler(arr) {
   const galleryItemMarkup = filmGalleryTpl(arr);
   filmGalleryRef.innerHTML = '';
   myLibrarySectionRef.classList.add('is-hiddenLib');
   filmGalleryRef.insertAdjacentHTML('beforeend', galleryItemMarkup);
-};
+}
 
 const fetchPopularMoviesList = () => {
   const url = `https://api.themoviedb.org/3/movie/popular?api_key=${api.API_KEY}&language=en-US&page=1`;
   return fetch(url)
     .then(response => response.json())
     .then(data => {
-      renderFilms = data.results;
+      const movies = data.results.map(item => ({
+        ...item,
+        release_date: cutReleaseDate(item.release_date),
+        genre_ids: drawGenres(allGenres, item.genre_ids),
+      }));
+
+      renderFilms = movies;
       const renderPages = [];
       renderFilms.forEach((item, index) => {
         if (index <= renderFilms.length / 9 + 1 && index !== 0) {
           renderPages.push(index);
         }
       });
+      let pageNumber = api.setPage();
       const popularFilms = true;
       localStorage.setItem('queryPopularFilms', popularFilms);
       pagesRenderHandler(renderPages);
@@ -62,26 +93,26 @@ const fetchPopularMoviesList = () => {
 
 fetchPopularMoviesList();
 
-export const apiService = {
-  key: '72466121c9676fc22348299f38033287',
-  searchQuery: '',
-  page: 1,
+function fetchGenres() {
+  const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${api.API_KEY}&language=en-US`;
+  return fetch(url)
+    .then(response => response.json())
+    .then(({ genres }) => {
+      return genres;
+    });
+}
 
-  fetchFilms(inputValue) {
-    const url = encodeURI(`https://api.themoviedb.org/3/search/movie?api_key=${this.key}&language=en-US&include_adult=false&query=${inputValue}`);
-    return fetch(url)
-      .then(response => response.json())
-      .then(data => data.results);
-  },
-  resetPage() {
-    this.page = 1;
-  },
-  get query() {
-    return this.searchQuery;
-  },
-  set query(newQery) {
-    this.searchQuery = newQery;
-  },
+fetchGenres();
+
+function cutReleaseDate(date) {
+  const cutReleaseDate = moment(date).format('YYYY');
+  return cutReleaseDate;
+}
+
+const drawGenres = (allGenres, renderGenres) => {
+  return renderGenres.map(genreId =>
+    allGenres.find(genre => genre.id === genreId),
+  );
 };
 
 export default createCardFunc;
